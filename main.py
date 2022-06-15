553b484d204229fbb6d641e06296a8df0d0f896d
Samuel Boccara <shboccara@gmail.com>
2022-06-15 21:23:01 +0000

    Finished hangman game.

--- main.py
+++ main.py
@@ -1,7 +1,104 @@
 """
-Name(s):
-Name of Project:
+Name(s): Sam Boccara
+Name of Project: Hangman
 """
+
+import random
+
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+#Word bank of animals
+words = ['ant', 'baboon', 'badger', 'bat', 'camel', 'otter', 'fox', 'mouse', 'mule', 'spider', 'wombat', 'zebra']
+
+guessProgress = []
+guessNumber = 0
+guessedLetters = []
+
+while guessNumber < 8:
+  if guessNumber == 0:
+    answer = words[random.randint(0, len(words) - 1)]
+    for i in range(len(answer)):
+      guessProgress.append("_")
+
+    guessNumber += 1
+  
+  print(HANGMANPICS[guessNumber - 1])
+  print("".join(guessProgress) + " Guessed Letters: " + "".join(guessedLetters) + " Mistakes Left: " + str(8 - guessNumber))
+  
+  guess = input("Guess a letter: ")
+  
+  for i in range(len(answer)):
+    if guess == answer[i]:
+        guessProgress[i] = guess
+
+  if guess not in answer:
+    guessedLetters.append(guess)
+    guessNumber += 1
+
+  if "".join(guessProgress) == answer:
+    print("Congratulations! You Won!")
+    play_again = input("Do you want to play again (y/n)? ")
+
+    if play_again == "n":
+      break
+    elif play_again == "y":
+      guessNumber = 0
+      guessProgress.clear()
+      continue
+
+  if guessNumber == 8:
+    print("You lose. The answer was", answer)
+      
+
 
 #Write the main part of your program here. Use of the other pages is optional.
 
